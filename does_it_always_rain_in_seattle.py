e512a2596d95616d5c7248ff884fa7d0d8b8c6d0
Violeta <violeta.bagaz@gmail.com>
2024-01-16 14:54:09 +0100

    .1 exercise

--- does_it_always_rain_in_seattle.py
+++ does_it_always_rain_in_seattle.py
@@ -1,13 +1,40 @@
 import json
-
 with open ('precipitation.json', encoding= 'utf-8') as file:
     precipitation = json.load(file)
 
+# Selecting Seattle out of the precipitation.json file and splitting the dates (to extract the month later on):
+    
 seattle_results = []
 for measurement in precipitation:
     city = measurement['station']
     if city == 'GHCND:US1WAKG0038':
         seattle_results.append(measurement)
+    measurement['date'] = measurement['date'].split('-')
 
+# Calculating the total monthly precipitation:
+    
+total_monthly_precipitation = {} 
+for month in seattle_results:
+    if month['date'][1] in total_monthly_precipitation:
+        total_monthly_precipitation[month['date'][1]] = total_monthly_precipitation[month['date'][1]] + month['value']
+    else:
+        total_monthly_precipitation[month['date'][1]] = month['value']
 
-print(seattle_results)
+total_monthly_precipitation_list = []
+for key, value in total_monthly_precipitation.items():
+    total_monthly_precipitation_list.append(value)
+
+# Saving the results to a result file:
+    
+results = {
+    'Seattle': {
+        'station': 'GHCND:US1WAKG0038',
+        'state': 'WA',
+        'total_monthly_precipitation': total_monthly_precipitation_list
+    }
+}
+
+with open ('results.json', 'w', encoding = 'utf-8') as file:
+    json.dump(results, file, indent = 3)
+
+print(results)
